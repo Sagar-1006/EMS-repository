d8bcdd72055d944e51bc982e4cbedc83b3dcbc3f
Kau <sagarsonawane088@gmail.com>
2022-08-13 08:58:48 +0530

    create EMS project

---
+++ EMSusingHibernate/src/main/java/com/BikkadIT/EmSusingHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmSusingHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmSusingHibernateApplication.class, args);
+	}
+
+}
